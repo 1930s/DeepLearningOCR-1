318c6a32d1122eebfb158c4da515a7fb9d7b1e1b
GitGeddes <hellohead102@gmail.com>
2018-11-02 18:36:50 -0400

    Fixed fontData folders not correct in Makefile, Added setup script

---
+++ dl/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Make sure to do "chmod +x setup.sh"
+# Run this with "sudo ./setup.sh"
+
+sudo apt-get update
+sudo apt-get install -y pkg-config
+sudo apt-get install -y gcc
+sudo apt-get install -y gtk+2.0
+sudo apt-get install -y libtiff5-dev
+sudo apt-get install -y python-dev
+sudo apt-get install -y ctags
+sudo apt-get install -y libpangox-1.0
