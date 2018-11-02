b9a0649f180553948fadd18d81eb5a50c802438a
GitGeddes <hellohead102@gmail.com>
2018-11-02 19:22:19 -0400

    Updated setup.sh

--- dl/setup.sh
+++ dl/setup.sh
@@ -8,6 +8,12 @@ sudo apt-get install -y pkg-config
 sudo apt-get install -y gcc
 sudo apt-get install -y gtk+2.0
 sudo apt-get install -y libtiff5-dev
-sudo apt-get install -y python-dev
 sudo apt-get install -y ctags
 sudo apt-get install -y libpangox-1.0
+
+sudo apt-get install -y python-dev
+sudo apt-get install -y python-pip
+pip install tensorflow
+# If that doesn't work, do "pip uninstall tensorflow" then
+# "pip install tensorflow==1.5"
+# This occurs on older CPUs
